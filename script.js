6cf41a282870dd4fc61e676d3e7ffa157d6d1be5
HIMOTHY-HIMMONS <vijval.yaramati.310@k12.friscoisd.org>
2024-08-08 17:26:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function cal() {
+    var input = document.getElementById("input").value;
+
+    if (input >= 18) {
+        alert("You are allowed to vote")
+    }
+
+    else {
+        alert("You still have to wait!")
+    }
+}
